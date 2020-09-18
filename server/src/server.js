27a479527ee54ddb913f36d2d647a1099695188e
Fernando Batista <nandosbx@gmail.com>
2020-09-17 23:58:10 -0300

    Config

--- server/src/server.js
+++ server/src/server.js
@@ -2,11 +2,25 @@ const express = require('express')
 const env = require('dotenv')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const config = require('./config/key')
 
 //Const
 env.config()
 
 app.use(express.json())
+
+mongoose
+    .connect(config.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch((err) => console.log(err))
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res, next) => {
     res.status(200).json({
