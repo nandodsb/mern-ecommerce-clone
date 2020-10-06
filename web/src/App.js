b632105a0646d4695a4942e83f3c062460fae99b
Fernando Batista <nandosbx@gmail.com>
2020-10-05 23:49:27 -0300

    Update

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import './App.css'
-import { Jumbotron } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown, Collapse } from 'react-bootstrap'
 
 function App() {
     return (
         <div className="App">
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">Admin Dashboard</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
                         <NavDropdown
                             title="Dropdown"
                             id="collasible-nav-dropdown"
