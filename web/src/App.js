7386ef7e821a7cf3d9e71627d043bf9888397870
Fernando Batista <nandosbx@gmail.com>
2020-10-10 00:00:37 -0300

    Container Structure

--- web/src/App.js
+++ web/src/App.js
@@ -5,7 +5,7 @@ import Layout from './components/Layout'
 function App() {
     return (
         <div className="App">
-            <Layout>Hello World</Layout>
+            
         </div>
     )
 }
