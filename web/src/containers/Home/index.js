23dab2e9aaf7127b654b9fe94e76e547ba615dbe
Fernando Batista <nandosbx@gmail.com>
2020-10-12 03:56:21 -0300

    Update

--- web/src/containers/Home/index.js
+++ web/src/containers/Home/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = (props) => {
     return (
         <>
-            <div>Signin</div>
+            <div>Home</div>
         </>
     )
 }
--- web/src/containers/Signup/index.js
+++ web/src/containers/Signup/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Signup = (props) => {
     return (
         <>
-            <div>Signin</div>
+            <div>Signup</div>
         </>
     )
 }
